443dd73458b9a16c98f361a6a4ba93c69a4864dd
Sidharth Shukla <swayam1044@gmail.com>
2023-06-29 21:05:06 +0530

    context done

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,23 @@
 import "../styles/app.scss"
 import Header from "./header"
+import { ContextProvider } from "../components/Clients"
 
 export const metadata = {
   title: 'Todo App',
   description: 'This is basic Todo App',
 }
- 
+
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
       <body>
-        <Header />
-        {children}
-        </body>
+        <ContextProvider>
+          <>
+            <Header />
+            {children}
+          </>
+        </ContextProvider>
+      </body>
     </html>
   )
 }
--- components/Clients.jsx
+++ components/Clients.jsx
@@ -1,11 +1,23 @@
 "use client";
 
+import { useState, createContext } from "react";
+export const Context = createContext({ user: {} });
+
+export const ContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  return (
+    <Context.Provider value={(user, setUser)}>{children}</Context.Provider>
+  );
+};
+
 export const LogoutBtn = () => {
+  const logoutHandler = () => {
+    alert("logout");
+  };
 
-
-    const logoutHandler = () =>{
-        alert("logout")
-    }
-
-    return <button className="btn" onClick={logoutHandler}>Logout</button>
-}
+  return (
+    <button className="btn" onClick={logoutHandler}>
+      Logout
+    </button>
+  );
+};
